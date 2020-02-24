90ac00713e49887cbd0c24cb2315a2fe47ea0a18
AurelioPa <aurelio.paolillo@gmail.com>
2020-02-24 11:04:08 +0100

    Move the axios work on server-side Create .JSON

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 
 import OnOff from "./OnOff";
 import LedButton from "./LedButton";
@@ -8,53 +7,9 @@ export default class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            ledOn: true,
-            address: "",
-            port: ""
+            ledOn: true
         };
         this.handleClick = this.handleClick.bind(this);
-        this.findGateway = this.findGateway.bind(this);
-        this.authenticate = this.authenticate.bind(this);
-    }
-
-    findGateway() {
-        axios
-            .get("https://dresden-light.appspot.com/discover")
-            .then(res => {
-                this.setState({
-                    address: res.data[0].internalipaddress,
-                    port: res.data[0].internalport
-                });
-            })
-            .then(() => {
-                this.authenticate();
-            })
-
-            .catch(err => {
-                console.info(err);
-            });
-    }
-
-    authenticate() {
-        this.state;
-        const pww = "Basic ZGVsaWdodDoxMDEwMTAxMA==";
-        const www =
-            "http://" +
-            this.state.address +
-            ":" +
-            this.state.port +
-            "/" +
-            "api";
-        axios({
-            method: "post",
-            url: www,
-            data: '{ devicetype: "myapp" }',
-            headers: { authorization: "Basic " + pww }
-        })
-            .then(res => console.info(res))
-            .catch(err => {
-                console.info(err);
-            });
     }
 
     handleClick() {
@@ -63,11 +18,6 @@ export default class App extends React.Component {
                 ledOn: !prevState.ledOn
             };
         });
-    }
-
-    componentDidMount() {
-        this.findGateway();
-        //this.authenticate();
     }
 
     render() {
--- src/server.js
+++ src/server.js
@@ -1,20 +1,68 @@
 import express from "express";
 import { port } from "./config";
+import axios from "axios";
+import fs from "fs";
+
 import serverRender from "renderers/server";
 
 const app = express();
+
+const findGateway = () => {
+    axios
+        .get("https://dresden-light.appspot.com/discover")
+        .then(res => {
+            authenticate(
+                res.data[0].internalipaddress,
+                res.data[0].internalport
+            );
+        })
+        .catch(err => {
+            console.info(err);
+        });
+};
+
+const authenticate = (iAddress, iPort) => {
+    const www = "http://" + iAddress + ":" + iPort + "/" + "api/";
+    const pww = "ZGVsaWdodDoxMDEwMTAxMA==";
+    const config = {
+        headers: { Authorization: "Basic " + pww }
+    };
+    axios
+        .post(www, { devicetype: "x" }, config)
+        .then(res => {
+            lightsRequest(www, res.data[0].success.username);
+        })
+        .catch(err => {
+            console.info(err, "Error on Authentication");
+        });
+};
+
+const lightsRequest = (www, username) => {
+    axios
+        .get(www + username + "/lights")
+        .then(res => {
+            fs.writeFile(
+                "src/lightsData.json",
+                JSON.stringify(res.data),
+                err => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.info("Replaced!");
+                }
+            );
+        })
+        .catch(e => {
+            console.info(e);
+        });
+};
 
 app.use(express.static("dist"));
 
 app.set("view engine", "ejs");
 
-app.all("/", (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-
 app.get("/", (req, res) => {
+    findGateway();
     const initialContent = serverRender();
     res.render("index", { initialContent });
 });
